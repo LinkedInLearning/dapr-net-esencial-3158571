a5e94d25a096725066effadcb466ea9d9bdd74db
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2021-12-08 21:09:30 -0600

    fix

--- src/WisdomPetMedicine.PetAggregator.Api/Program.cs
+++ src/WisdomPetMedicine.PetAggregator.Api/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
